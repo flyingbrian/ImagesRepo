ac405f97ffef8c90b847ce4f926104fe403fa18e
B <flyingbrian23@gmail.com>
2017-07-13 01:03:45 -0400

    updated controls

--- pixiJSTest/js/input.js
+++ pixiJSTest/js/input.js
@@ -1,7 +1,8 @@
 var rightPressed;
 var leftPressed;
+var upPressed;
+var downPressed;
 var spaceBarPressed;
-
 
 document.addEventListener('keydown', function(event)
 {
@@ -13,6 +14,16 @@ document.addEventListener('keydown', function(event)
 	{
 		rightPressed = true;
 	}
+
+	if (event.keyCode == 38) 
+	{
+		upPressed = true;
+	}
+	else if (event.keyCode == 40) 
+	{
+		downPressed = true;
+	}
+
 
 	if(event.keyCode == 32)
 	{
@@ -30,6 +41,16 @@ document.addEventListener('keyup', function(event)
 	else if(event.keyCode == 39)
 	{
 		rightPressed = false;
+	}
+
+
+	if (event.keyCode == 38) 
+	{
+		upPressed = false;
+	}
+	else if (event.keyCode == 40) 
+	{
+		downPressed = false;
 	}
 
 	if(event.keyCode == 32)
---
+++ pixiJSTest/js/mathUtil.js
@@ -0,0 +1,9 @@
+function RADS2DEGS(rads)
+{
+	return rads * (180/Math.PI);
+}
+
+function DEGS2RADS(degrees)
+{
+	return rads * (ath.PI/180);
+}
